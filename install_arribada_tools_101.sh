6ee7f40e3c493972827ed2a68194d2da41083d1b
Cris Thompson <cris@octophin.com>
2020-02-14 14:58:57 +0000

    Update install_arribada_tools_101.sh

--- install_arribada_tools_101.sh
+++ install_arribada_tools_101.sh
@@ -1,1 +1,18 @@
 # Install Arribada Tools v1.0.1 compatible with STM32-based Arribada Horizon tags.
+
+# system up to date
+sudo apt update
+sudo apt upgrade
+
+# install required software
+sudo apt install unzip openssh-server python2.7 python-setuptools build-essential libgtk2.0-dev
+
+# allow ssh access
+sudo systemctl enable ssh
+
+# arribada tools
+curl -L -o 1.0.1.zip  "https://github.com/arribada/horizon-v2-tools/archive/1.0.1.zip"
+unzip 1.0.1.zip
+cd horizon-v2-tools-1.0.1
+
+sudo python setup.py install
